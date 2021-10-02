03e1a73c21c4b55d4cb1f0af821f1a2bf730a35b
Andrei <Andrei@example.com>
2021-10-02 14:52:44 +0300

    New version

---
+++ index_botanic.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Flower Gallery</title>
+        <link rel="stylesheet" type="text/css" href="css/style_botanic.css"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        <div class="continut">
+            <div class="header">
+                <h1>Flower gallery</h1>
+                <p>Here is a selection of our botanical prints.</p>
+            </div>
+            
+            <div class="piesa">
+                
+                <figure>
+                    <img src="imagini/imagini_botanic/poppy.jpg" alt="Poppy"/>
+                    <figcaption>Poppy</figcaption>
+                </figure>
+                
+            </div>
+            
+            <div class="piesa">
+                <figure>
+                    <img src="imagini/imagini_botanic/selenicereus.jpg" alt="Selenicereus"/>
+                    <figcaption>Selenicereus</figcaption>
+                </figure>
+            </div>
+         
+            <div class="piesa">
+                <figure>
+                    <img src="imagini/imagini_botanic/narcisus.jpg" alt="Narcisus"/>
+                    <figcaption>Narcisus</figcaption>
+                </figure>
+            </div>
+            
+           
+            <div class="piesa">
+                <figure>
+                    <img src="imagini/imagini_botanic/rhododendron.jpg" alt="Rhododendron"/>
+                    <figcaption>Rhododendron</figcaption>
+                </figure>
+            </div>
+            
+            <div class="piesa">
+                <figure>
+                    <img src="imagini/imagini_botanic/hydrangea.jpg" alt="Hydrangea"/>
+                    <figcaption>Hydrangea</figcaption>
+                </figure>
+            </div>
+            
+            <div class="piesa">
+                <figure>
+                    <img src="imagini/imagini_botanic/magnolia.jpg" alt="Magnolia"/>
+                    <figcaption>Magnolia</figcaption>
+                </figure>
+            </div>
+
+        </div>
+    </body>
+</html>
